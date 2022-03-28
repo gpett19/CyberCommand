7fcad0413a060190fb2fc20d68506e18778b73ae
Kali <cailemieuxmack@gmail.com>
2022-03-28 11:27:30 -0400

    added reading commands from files - does not work

--- multiThreadingExample.py
+++ multiThreadingExample.py
@@ -3,36 +3,47 @@ import socketserver
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 
-    def handle(self):
-        data = str(self.request.recv(1024), 'ascii')
-        cur_thread = threading.current_thread()
-        response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
-        self.request.sendall(response)
-        while True:
-                #Now, let's figure out how to get it to send commands instead!
-                command =input("Enter a command for {}:\n".format(cur_thread.name))
-                self.request.sendall(command.encode())
-                message = self.request.recv(1024).strip().decode()
-                response = bytes("{}: {}".format(cur_thread.name, message), 'ascii').decode()
-                print(response)
+	def handle(self):
+		data = str(self.request.recv(1024), 'ascii')
+		cur_thread = threading.current_thread()
+		response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
+		self.request.sendall(response)
+		while True:
+			thread_num = cur_thread.name.split("-")[-1]
+			with open(thread_num + ".txt", 'wr') as file:
+				
+			#Now, let's figure out how to get it to send commands instead!
+				command = file.readline()
+				self.request.sendall(command.encode())
+				message = self.request.recv(1024).strip().decode()
+				response = bytes("{}: {}".format(cur_thread.name, message), 'ascii').decode()
+				print(response)
+				file.truncate(0)
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    pass
+	pass
 if __name__ == "__main__":
-    # Port 0 means to select an arbitrary unused port
-    HOST, PORT = "", 8000
+	# Port 0 means to select an arbitrary unused port
+	HOST, PORT = "", 8000
 
-    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
-    socketserver.TCPServer.allow_reuse_address = True
-    with server:
-        ip, port = server.server_address
+	server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+	socketserver.TCPServer.allow_reuse_address = True
+	with server:
+		ip, port = server.server_address
 
-        # Start a thread with the server -- that thread will then start one
-        # more thread for each request
-        server_thread = threading.Thread(target=server.serve_forever())
-        # Exit the server thread when the main thread terminates
-        server_thread.daemon = True
-        server_thread.start()
-        print("Server loop running in thread:", server_thread.name)
+		# Start a thread with the server -- that thread will then start one
+		# more thread for each request
+		server_thread = threading.Thread(target=server.serve_forever())
+		# Exit the server thread when the main thread terminates
+		server_thread.daemon = True
+		server_thread.start()
+		print("Server loop running in thread:", server_thread.name)
 
-        #server.shutdown()
+	while true:
+		command = input("enter a command: ")
+		bots = input("which bots should run this?").split(",")
+		for bot in bots:
+			with open(bot + ".txt", 'w') as file:
+				file.write(command)
+
+	#server.shutdown()
